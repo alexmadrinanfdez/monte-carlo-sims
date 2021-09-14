a9e1f2e76ceb5af5931c6483f1f20e9fe3933df5
Alejandro Madriñán <alejandro.madrinanfer@alumnos.upm.es>
2021-09-13 20:02:32 -0500

    bus without loop

--- bus_schedule.m
+++ bus_schedule.m
@@ -3,7 +3,7 @@
 % minutes past the hour, at 30 minutes past the hour, and at 45 minutes
 % past the hour. However, due to random fluctuations, it arrives anywhere
 % between 1 minute early and 2 minutes late with uniform distribution
-% (\(\mathcal{U}\(-2, 1\)\). Assume that the arrivals of different buses 
+% ( $\mathcal{U}(-1, 2)$ ). Assume that the arrivals of different buses 
 % are independent and identically distributed (IID).
 
 %% Parameters
@@ -15,47 +15,28 @@ rep = 1e6;   % number of repetitions
 
 %% Simulation
 % The Monte-Carlo simulation will assume people arrive at the bus stop
-% randomly, one person every time a bus arrives \mathcal{U}\(0, 15\).
+% randomly, one person every time a bus arrives ( $\mathcal{U}(0, 15)$ ).
 % Passengers will enter the first bus that arrives. There is no problem of
 % space inside the bus.
+% Furthermore, we will use the inherent symmetry of the problem to rule out
+% one of the posibilities: the passenger arrives after the first bus has
+% already passed. Time is centered on the passenger.
 
-schedule = (1:rep)' * t_int;
 span_d = dhi - dlo;
-delay = rand(rep, 1) * span_d + dlo;
-stops = schedule + delay;
-arrivals = rand(rep, 1) * t_int + schedule - t_int;
-waitt = (1:rep)';
+unif = rand(rep, 3);
+y = unif(:, 1) * t_int;
+x_2 = unif(:, 2) * span_d + t_int + dlo - dhi;
+x_3 = unif(:, 3) * span_d + 2*t_int + dlo - dhi;
 
-% scales exponentially
-% tic;
-% for i = 1:rep
-%     for j = 1:rep
-%         if arrivals(i) < stops(j)
-%             waitt(i) = stops(j) - arrivals(i);
-%             break
-%         end
-%     end
-% end
-% toc;
-
-% scales linearly
 tic;
-for i = 1:rep
-    if i == 1
-        dif = stops(i:i+1) - arrivals(i);
-    elseif i == rep
-        dif = stops(i-1:i) - arrivals(i);
-    else
-        dif = stops(i-1:i+1) - arrivals(i);
-    end
-    pos = dif(dif > 0);
-    waitt(i) = min(pos);
-end
+waitt = (x_2 - y).*(x_2 >= y) + (x_3 - y).*(x_2 < y);
 toc;
+
+muhat = mean(waitt);
 
 %% Output results
 
-disp(['The average waiting time obtained is ' num2str(mean(waitt))])
+disp(['The average waiting time obtained is ' num2str(muhat)])
 
 %%
 % _Author: Alejandro Madriñán Fernández_
