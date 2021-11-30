afe3b6916ab7c267824b855f7f618395b95fd3c2
Alejandro Madriñán <alejandro.madrinanfer@alumnos.upm.es>
2021-11-29 19:16:11 -0600

    auxiliar sampling functions for option pricing

---
+++ YoptPrice_cv.m
@@ -0,0 +1,10 @@
+function YCV=YoptPrice_cv(optPayoffObj,n)
+% YOPTPRICE_CV creates the control variate output for option pricing using
+% the |optPayoff| object
+
+meanX = optPayoffObj.exactPrice(2:end); %get the exact option prices for the control variates
+YX = genOptPayoffs(optPayoffObj,n); %get the Y and X values
+beta = bsxfun(@minus,YX(:,2:end),mean(YX(:,2:end),1))\YX(:,1); %optimal beta
+YCV = YX(:,1) - bsxfun(@minus,YX(:,2:end),meanX)*beta; %control variate random variable
+end
+
